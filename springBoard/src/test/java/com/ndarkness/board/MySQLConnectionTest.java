720c63ae44d9d8813058195ccc727233b2dc5300
P110890 <P110890@NSP110890.sktsoc.ad>
2018-05-16 17:22:25 +0900

    수정

--- springBoard/src/test/java/com/ndarkness/board/MySQLConnectionTest.java
+++ springBoard/src/test/java/com/ndarkness/board/MySQLConnectionTest.java
@@ -9,7 +9,7 @@ import java.sql.Connection;
 public class MySQLConnectionTest {
 
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:3306/ndarkness?serverTimezone=UTC&&useSSL=false";
+	private static final String URL = "jdbc:mysql://localhost:3306/ndarkness?serverTimezone=UTC&useSSL=false";
 	//jdbc:mysql:주소:포트/DB명
 	private static final String USER = "root";
 	private static final String PW = "123456";
