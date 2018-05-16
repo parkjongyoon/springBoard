6dd01938bd85f1a6f1827dab2cf5afabb86353a1
P110890 <P110890@NSP110890.sktsoc.ad>
2018-05-16 10:02:19 +0900

    첫번째 커밋

---
+++ springBoard/src/test/java/com/ndarkness/board/MySQLConnectionTest.java
@@ -0,0 +1,28 @@
+package com.ndarkness.board;
+
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+
+public class MySQLConnectionTest {
+
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/ndarkness?serverTimezone=UTC&&useSSL=false";
+	//jdbc:mysql:주소:포트/DB명
+	private static final String USER = "root";
+	private static final String PW = "123456";
+	//Root 비밀번호
+	
+	@Test
+	public void testConnection() throws Exception{
+		Class.forName(DRIVER);
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			System.out.println(con);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
